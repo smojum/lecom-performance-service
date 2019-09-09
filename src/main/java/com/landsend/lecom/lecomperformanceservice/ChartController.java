fc4fea2981ee717a7ecc986845bee13febf6bc47
smojumd <santanu.mojumder@landsend.com>
2019-09-09 16:20:51 -0500

    added dropdown for now. need to change this to overlap.

--- src/main/java/com/landsend/lecom/lecomperformanceservice/ChartController.java
+++ src/main/java/com/landsend/lecom/lecomperformanceservice/ChartController.java
@@ -5,6 +5,9 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import java.util.List;
 
 @Controller()
 @Slf4j
@@ -13,9 +16,24 @@ public class ChartController {
     @Autowired
     private PerformanceMetricsRepository repository;
     @GetMapping("/chart")
-    public String chart(Model model) {
-        model.addAttribute("message", "Testing");
-        model.addAttribute("data", repository.findAll());
+    public String chart(@RequestParam(value = "baseUrl", required = false) String baseUrl, Model model) {
+        model.addAttribute("urls", getUrls());
+        if(baseUrl == null) {
+            model.addAttribute("data", null);
+        } else {
+            log.info("Getting data for url: " + baseUrl);
+            List<PerformanceMetrics> result = repository.findByUrlContaining(baseUrl);
+            log.info("Result: " + result);
+            model.addAttribute("data", result);
+        }
+        model.addAttribute("baseUrl", baseUrl);
         return "chart";
+    }
+    private String[] getUrls() {
+        String[] urls = new String[3];
+        urls[0] = "";
+        urls[1] = "le-tst-2.landsend.com";
+        urls[2] = "le-int-c.landsend.com";
+        return urls;
     }
 }
