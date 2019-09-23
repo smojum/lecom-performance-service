37506fca5adc1bbb650b84353c6e55f5a9413980
smojumd <santanu.mojumder@landsend.com>
2019-09-23 15:23:43 -0500

    overlapping charts - done with data. will implement from mongodb now.

--- src/main/java/com/landsend/lecom/lecomperformanceservice/ChartController.java
+++ src/main/java/com/landsend/lecom/lecomperformanceservice/ChartController.java
@@ -58,7 +58,7 @@ public class ChartController {
 
 
     @GetMapping("/chart-all")
-    public String chartall(Model model) {
+    public String chartAll(Model model) {
         List<PerformanceMetrics> result = repository.findAll();
         result.stream()
                 .forEach(performanceMetrics -> performanceMetrics.setHtml(null));
@@ -72,16 +72,19 @@ public class ChartController {
         return "chart-all";
     }
 
-    private Map<String, List<List<String>>> getChartData() {
+    private Map<String, Map<String, Map<String, List<List<String>>>>> getChartData() {
         List<String> datapoint1_1 = new ArrayList<>();
         datapoint1_1.add("2019-09-19T16:44:18.473");
         datapoint1_1.add("156");
+        datapoint1_1.add(UUID.randomUUID().toString());
         List<String> datapoint1_2 = new ArrayList<>();
         datapoint1_2.add("2019-09-19T16:54:18.473");
         datapoint1_2.add("166");
+        datapoint1_1.add(UUID.randomUUID().toString());
         List<String> datapoint1_3 = new ArrayList<>();
         datapoint1_3.add("2019-09-19T17:04:18.473");
         datapoint1_3.add("176");
+        datapoint1_1.add(UUID.randomUUID().toString());
         List<List<String>> datapoint_1 = new ArrayList<>();
         datapoint_1.add(datapoint1_1);
         datapoint_1.add(datapoint1_2);
@@ -89,12 +92,15 @@ public class ChartController {
         List<String> datapoint2_1 = new ArrayList<>();
         datapoint2_1.add("2019-09-19T16:45:18.473");
         datapoint2_1.add("159");
+        datapoint1_1.add(UUID.randomUUID().toString());
         List<String> datapoint2_2 = new ArrayList<>();
         datapoint2_2.add("2019-09-19T16:55:18.473");
         datapoint2_2.add("169");
+        datapoint1_1.add(UUID.randomUUID().toString());
         List<String> datapoint2_3 = new ArrayList<>();
         datapoint2_3.add("2019-09-19T17:05:18.473");
         datapoint2_3.add("179");
+        datapoint1_1.add(UUID.randomUUID().toString());
         List<List<String>> datapoint_2 = new ArrayList<>();
         datapoint_2.add(datapoint2_1);
         datapoint_2.add(datapoint2_2);
@@ -104,7 +110,19 @@ public class ChartController {
 
         group.put("le-int-b.landsend.com", datapoint_1);
         group.put("le-int-c.landsend.com", datapoint_2);
-        return group;
+        Map<String, Map<String, List<List<String>>>> type = new HashMap();
+        Map<String, List<List<String>>> group2 = (Map<String, List<List<String>>>) ((HashMap) group).clone();
+        Map<String, List<List<String>>> group3 = (Map<String, List<List<String>>>) ((HashMap) group).clone();
+        type.put("FCP", group);
+        type.put("Interactive", group2);
+        type.put("SI", group3);
+        Map<String, Map<String, Map<String, List<List<String>>>>> allMap = new HashMap<>();
+        Map<String, Map<String, List<List<String>>>> type2 = (Map<String, Map<String, List<List<String>>>>) ((HashMap)type).clone();
+        Map<String, Map<String, List<List<String>>>> type3 = (Map<String, Map<String, List<List<String>>>>) ((HashMap)type).clone();
+        allMap.put("PDP", type);
+        allMap.put("PLP", type2);
+        allMap.put("Other", type3);
+        return allMap;
     }
     private String[] getUrls() {
         String[] urls = new String[3];
