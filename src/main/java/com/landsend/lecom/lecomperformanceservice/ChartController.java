08794d96eb28bc5a777e71e119c9268272a04862
smojumd <santanu.mojumder@landsend.com>
2019-10-22 16:01:24 -0500

    different servers.

--- src/main/java/com/landsend/lecom/lecomperformanceservice/ChartController.java
+++ src/main/java/com/landsend/lecom/lecomperformanceservice/ChartController.java
@@ -22,7 +22,7 @@ public class ChartController {
     @Autowired
     private PerformanceMetricsRepository repository;
 
-    private List<String> staticDomains = Arrays.asList("www", "origin-m1-www", "origin-m1-www", "le-qas-a", "le-int-c");
+    private List<String> staticDomains = Arrays.asList("www", "origin-m1-www", "origin-d1-www", "le-qas-a", "le-int-c");
 
     @GetMapping("/chart")
     public String chart(@RequestParam(value = "selectedDays", required = false, defaultValue = "3") Integer selectedDays,
