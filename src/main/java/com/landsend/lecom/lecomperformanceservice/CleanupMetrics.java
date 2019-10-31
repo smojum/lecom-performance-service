f88e071941ea7132434820e3f03a98259a0b7756
smojumd <santanu.mojumder@landsend.com>
2019-10-31 11:02:56 -0500

    new configs.

--- src/main/java/com/landsend/lecom/lecomperformanceservice/CleanupMetrics.java
+++ src/main/java/com/landsend/lecom/lecomperformanceservice/CleanupMetrics.java
@@ -2,6 +2,7 @@ package com.landsend.lecom.lecomperformanceservice;
 
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Component;
 
@@ -14,10 +15,13 @@ public class CleanupMetrics {
     @Autowired
     private PerformanceMetricsRepository repository;
 
+    @Value("${cleanup.days}")
+    private Integer cleanupDays;
+
     @Scheduled(fixedRate = 500000)
     public void cleanup() {
         log.info("Cleaning up old data from metrics");
-        repository.deleteAll(repository.findByRunTimeLte(LocalDateTime.now(Clock.systemUTC()).minusDays(3).minusHours(0)));
+        repository.deleteAll(repository.findByRunTimeLte(LocalDateTime.now(Clock.systemUTC()).minusDays(cleanupDays).minusHours(0)));
         log.info("Cleaned up metrics successfully");
     }
 }
--- src/main/java/com/landsend/lecom/lecomperformanceservice/PerformanceMetrics.java
+++ src/main/java/com/landsend/lecom/lecomperformanceservice/PerformanceMetrics.java
@@ -3,6 +3,7 @@ package com.landsend.lecom.lecomperformanceservice;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import lombok.*;
 import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
 import org.springframework.data.mongodb.core.mapping.Document;
 
 import java.net.MalformedURLException;
@@ -25,6 +26,7 @@ public class PerformanceMetrics {
     private Integer interactive;
     private Integer speedIndex;
     @JsonFormat(timezone = "CST")
+    @Indexed
     private LocalDateTime runTime;
     private String html;
 
